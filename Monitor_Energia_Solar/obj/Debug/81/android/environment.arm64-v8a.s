899aaa21c74c266f8d935ad32b067834a4a47708
DomingosOctavio <tavotere@hotmail.com>
2022-08-27 02:09:27 -0300

    Ajustes Layout Button Login

--- Monitor_Energia_Solar/obj/Debug/81/android/environment.arm64-v8a.s
+++ Monitor_Energia_Solar/obj/Debug/81/android/environment.arm64-v8a.s
@@ -1170,7 +1170,7 @@ application_config:
 
 	.type	.L.autostr.6, @object
 .L.autostr.6:
-	.asciz	"fc223ed1-9684-4897-bc64-102a4e381ce7"
+	.asciz	"38fbea5a-c8e6-4ef5-b555-f0d2c91cc2b1"
 	.size	.L.autostr.6, 37
 
 	.type	.L.autostr.7, @object
