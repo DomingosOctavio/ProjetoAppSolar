4e36f24445f58b6fb5d508f9ad6ea0b8aafd29e6
DomingosOctavio <tavotere@hotmail.com>
2022-09-10 22:35:45 -0300

    Ajuste bug Login

--- Monitor_Energia_Solar/ViewA/Login.cs
+++ Monitor_Energia_Solar/ViewA/Login.cs
@@ -87,18 +87,35 @@ namespace Monitor_Energia_Solar
         
 
         private void Btnsave_Click(object sender, EventArgs e)
-        {
-            LayoutInflater layoutInflater = LayoutInflater.From(this);
-            View view = layoutInflater.Inflate(Resource.Layout.Aguarde, null);
-            Android.Support.V7.App.AlertDialog.Builder alertbuilder = new Android.Support.V7.App.AlertDialog.Builder(this);
-            alertbuilder.SetView(view);
-            Android.Support.V7.App.AlertDialog dialog = alertbuilder.Create();
-            dialog.Show();
+       {
+        //    LayoutInflater layoutInflater = LayoutInflater.From(this);
+        //    View view = layoutInflater.Inflate(Resource.Layout.Aguarde, null);
+        //    Android.Support.V7.App.AlertDialog.Builder alertbuilder = new Android.Support.V7.App.AlertDialog.Builder(this);
+        //    alertbuilder.SetView(view);
+        //    Android.Support.V7.App.AlertDialog dialog = alertbuilder.Create();
+        //    dialog.Show();
 
             Obj_Banco_Dados obj_Banco = new Obj_Banco_Dados();
             LoginController loginControler = new LoginController();
+            try
+            {
+                obj_Banco = loginControler.BuscardadosLogin(usuario.Text, senha.Text);
+            }
+            catch(Exception ex)
+            {
+                Android.App.AlertDialog.Builder alert = new Android.App.AlertDialog.Builder(this);
+                alert.SetTitle("Atenção");
+                alert.SetIcon(Resource.Drawable.sair);
+                alert.SetMessage("Ocorreu um erro ao acessar o banco de dados. Erro: " + ex.Message);
 
-            obj_Banco = loginControler.BuscardadosLogin(usuario.Text, senha.Text);
+                alert.SetPositiveButton("sim", (senderAlert, args) =>
+                {
+                    this.FinishAffinity();
+                });
+
+              
+            }
+          
 
 
 
@@ -128,7 +145,7 @@ namespace Monitor_Energia_Solar
                         var intent = new Intent(this, typeof(MainActivity));
                         intent.SetFlags(ActivityFlags.NewTask);
                         //Navigation to SecondActivity
-                        alertbuilder.Dispose();
+                        //alertbuilder.Dispose();
                         StartActivity(intent);
                         //delete main activity from navigation
                         Finish();
